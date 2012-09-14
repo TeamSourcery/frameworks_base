c5b2222d3501df045a336b9511b7bc36d8f87cae
Eagle1967 <icsourcery@gmail.com>
2012-09-14 02:02:29 -0600

    add working lte button to cm toggles

--- packages/SystemUI/src/com/android/systemui/statusbar/powerwidget/PowerButton.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/powerwidget/PowerButton.java
@@ -49,7 +49,7 @@ public abstract class PowerButton {
     public static final String BUTTON_MEDIA_PLAY_PAUSE = "toggleMediaPlayPause";
     public static final String BUTTON_MEDIA_PREVIOUS = "toggleMediaPrevious";
     public static final String BUTTON_MEDIA_NEXT = "toggleMediaNext";
-   // public static final String BUTTON_LTE = "toggleLte";
+    public static final String BUTTON_LTE = "toggleLte";
    // public static final String BUTTON_WIMAX = "toggleWimax";
     public static final String BUTTON_UNKNOWN = "unknown";
     private static final String SEPARATOR = "OV=I=XseparatorX=I=VO";
