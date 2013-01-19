d38deb53eab15d8ceaf00c00d04117775ab43f30
eagle1967 <icsourcery@gmail.com>
2013-01-19 16:46:46 -0700

    add lockscreen shortcuts PA style

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2234,6 +2234,14 @@ public final class Settings {
          */
         public static final String LOCKSCREEN_TARGETS = "lockscreen_targets";
 
+        /**  
+ 	 * Defines the shortcuts to be shown on lockscreen
+         * Usage is like this: target:icon|target:icon|target:icon
+         * if :icon is not set, default application icon will be used
+         * @hide
+         */
+         public static final String LOCKSCREEN_SHORTCUTS = "lockscreen_shortcuts";
+
         /**
          * @deprecated Use {@link android.provider.Settings.Global#LOW_BATTERY_SOUND}
          * instead
